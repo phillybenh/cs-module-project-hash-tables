872e8b879f7cf97fc3c7136575b45ea2009e4c8d
Ben Haus <ben@little-haus.com>
2020-07-09 17:54:44 -0400

    regex to filter special characters working

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,32 @@
-# Your code here
+import re
 
+def histogram(file_name):
+
+    # Read in all the words in one go
+    with open(file_name) as f:
+        words = f.read()
+
+    # Case should be ignored. Output keys must be lowercase.
+    words = words.lower()
+    """
+    # Ignore each of the following characters:
+    ignore = ['"', ':', ';', ',', '.', '-', '+',
+              '=', '/', "\\", '|', '[', ']', '{',
+              '}', '(', ')', '*', '^', '&']
+
+    # for char in ignore:
+    #     words = words.replace(char, "")
+    """
+
+    #implementing with regex
+    words = re.sub(r'[^a-z\']', ' ', words).split()
+
+    count = {}
+    for w in words:
+        if w not in count:
+            count[w] = 0
+
+        count[w] += 1
+    return count
+
+print(histogram("robin.txt"))
